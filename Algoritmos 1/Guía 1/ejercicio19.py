fbc242ca6be736f21a61b08458e1746f877de3c1
aleksLlanos <alexanderllanos321@gmail.com>
2026-08-30 21:22:04 -0300

    hice los ejercicios 17, 18 y 19, y el 19 me salió pro

---
+++ Algoritmos 1/Guía 1/ejercicio19.py
@@ -0,0 +1,14 @@
+"""Ejercicio 19. Sean p, q y r tres variables de las que se sabe que:
+
+. P y Q nunca están indefinidas,
+. R se indefine si q es verdadera.
+
+Proponer, para cada ítem, una fórmula que nunca se indefina, utilizando siempre las tres variables. Cada fórmula debe
+ser verdadera si y solo sí se cumple que:
+
+a) Al menos una es verdadera. # (P or Q) or 
+b) Ninguna es verdadera.
+c) Exactamente una de las tres es verdadera.
+d) Sólo p y q son verdaderas.
+e) No todas al mismo tiempo son verdaderas.
+f) r es verdadera."""
